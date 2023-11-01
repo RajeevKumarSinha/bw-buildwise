a56ff84e5aca5e9a035b3cb70972386fd0fa3dbc
RajeevKumarSinhakoder <rajeev.exousia@gmail.com>
2023-11-01 16:23:42 +0530

    fix: Changed update & save data to TitleCase

--- models/connectionTypeModel.js
+++ models/connectionTypeModel.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const mongoose = require("mongoose")
-const { errObject } = require("../helpers/helper")
+const { errObject, titleCase } = require("../helpers/helper")
 
 const connectionTypeSchema = new mongoose.Schema({
 	connectionTypeName: {
@@ -15,15 +15,6 @@ const connectionTypeSchema = new mongoose.Schema({
 		required: true,
 		maxLength: 8,
 	},
-})
-
-connectionTypeSchema.pre("findOneAndUpdate", async function (next) {
-	const toUpdate = findOne(this._conditions._id._id)
-
-	// return a error if toUpdate is null
-	if (!toUpdate) return next(errObject("Invalid Id", 400))
-
-	next()
 })
 
 const ConnectionType = mongoose.model("ConnectionType", connectionTypeSchema)
