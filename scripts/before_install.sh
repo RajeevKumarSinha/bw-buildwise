c57caede17ec8e46569809ca58fb991abbef8411
Rajeev Kumar <77339429+RajeevKumarSinha@users.noreply.github.com>
2024-01-03 00:14:29 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,10 +6,10 @@ curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install -y nodejs
 
 # Create the deployment directory if it doesn't exist
-mkdir -p /var/www/your-backend
+mkdir -p /var/www/bw-buildwise
 
 # Change directory to the deployment path
-cd /var/www/your-backend || exit 1
+cd /var/www/bw-buildwise || exit 1
 
 # Install project dependencies
 npm install
