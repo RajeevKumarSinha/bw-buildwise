a985ecb63b2769bc6303278d9a596bc52a85d122
RajeevKumarSinhakoder <rajeev.exousia@gmail.com>
2024-01-03 01:41:59 +0530

    fix: final-time

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
+#Stopping existing node servers
 echo "Stopping any existing node servers"
-pid=$(pgrep node)
-if [ -n "$pid" ]; then
-    echo "Killing Node.js processes with PID: $pid"
-    sudo kill $pid
-else
-    echo "No Node.js processes found"
-fi
+sudo pkill node
